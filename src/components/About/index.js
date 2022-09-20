a71b9059e1faf71304df8086eeef95f7b8f0db23
Sean M <sean.maddox89@yahoo.com>
2022-09-20 03:56:10 -0700

    about wip

---
+++ src/components/About/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 className="asset">Why I would be an asset for your company</h1>
+      <div>
+        <p>
+          I am someone who is competitve which helps me strive to become better. I am a team player and will 
+          take on any tasks that will benefit the group. I am a hard worker who is always looking to take on
+          new tasks or challenges. 
+        </p>
+      </div>
+      <div className="knowledge">
+        <h2>Skills that I can offer</h2>
+        <p>
+          I have experience with learning the following coding languages:
+          HTML
+          CSS
+          JavaScript
+          Node
+          MySql
+          Mongo
+          Bash
+          NoSql
+          </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
