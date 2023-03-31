b99aadbedddd1c1101618ad7790576e0b00d68a8
Sean M <sean.maddox89@yahoo.com>
2023-03-31 04:48:22 -0700

    added apps to portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import Image1 from "../../Assets/pictures/random.pw.png";
 import Image2 from "../../Assets/pictures/dose.png";
 import Image3 from "../../Assets/pictures/rhythm.in.region.png";
+import Image4 from "../../Assets/pictures/social.network.png";
+import Image5 from "../../Assets/pictures/team.profile.png";
+import Image6 from "../../Assets/pictures/chore.it.up.png";
 import {
   MDBCard,
   MDBCardImage,
@@ -25,7 +28,7 @@ function Portfolio() {
       <h2 className="mt-3">Portfolio</h2>
       <MDBRow className="row-cols-1 row-cols-md-3 g-4">
         <MDBCol>
-          <MDBCard>
+          <MDBCard className="h-100">
             <MDBCardImage
               src={Image1}
               alt="Random Password Generator"
@@ -50,7 +53,7 @@ function Portfolio() {
           </MDBCard>
         </MDBCol>
         <MDBCol>
-          <MDBCard>
+          <MDBCard className="h-100">
             <MDBCardImage src={Image2} alt="Dose" position="top" />
             <MDBCardBody>
               <MDBCardTitle>Dose</MDBCardTitle>
@@ -71,7 +74,7 @@ function Portfolio() {
           </MDBCard>
         </MDBCol>
         <MDBCol>
-          <MDBCard>
+          <MDBCard className="h-100">
             <MDBCardImage src={Image3} alt="Rhythm in Region" position="top" />
             <MDBCardBody>
               <MDBCardTitle>Rhythm in Region</MDBCardTitle>
@@ -87,6 +90,91 @@ function Portfolio() {
             <MDBBtn
               onClick={() =>
                 openNewTab("https://bin-ostrowski.github.io/rhythm-in-region/")
+              }
+            >
+              View App
+            </MDBBtn>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol className="mt-3">
+          <MDBCard className="h-100">
+            <MDBCardImage src={Image4} alt="Social Network" position="top" />
+            <MDBCardBody>
+              <MDBCardTitle>Social Network</MDBCardTitle>
+              <MDBCardText>
+                Social Network is not a traditional application, but rather a
+                demonstation when using Express.js, MongoDB, and Node.js. In
+                Social Network users are able to sign up, update their
+                information, find another user by searching all or by id, and
+                delete their account/information. Also users can add and delete
+                friends. Once they are friends with another user they can
+                respond to their posts just like in any other social media
+                platform. To view a demonstation with Insomnia click on the view
+                app button.
+              </MDBCardText>
+            </MDBCardBody>
+            <MDBBtn
+              onClick={() =>
+                openNewTab(
+                  "https://drive.google.com/file/d/1cDPaayDMDWUK4X05iX_WSMsab1t74rKk/view"
+                )
+              }
+            >
+              View App
+            </MDBBtn>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol className="mt-3">
+          <MDBCard className="h-100">
+            <MDBCardImage
+              src={Image5}
+              alt="Team Profile Generator"
+              position="top"
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Team Profile Generator</MDBCardTitle>
+              <MDBCardText>
+                In Team Profile Generator employers are able to enter their
+                employee's information such as their name, email, role in the
+                company, as well as their link to their GitHub if needed. As you
+                enter the each employees information it will prompt the user if
+                there is another employee that they would like to enter. Once
+                all the employee's are entered in a page will populate with the
+                information provided. This application was created using
+                Node.js, inquirer, as well as jest. Click on the view app button
+                to see a video demonstation of the application.
+              </MDBCardText>
+            </MDBCardBody>
+            <MDBBtn
+              onClick={() =>
+                openNewTab(
+                  "https://drive.google.com/file/d/1-avsrQTqfsRWvzHJhv2d98vCLrSboiFg/view"
+                )
+              }
+            >
+              View App
+            </MDBBtn>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol className="mt-3">
+          <MDBCard className="h-100">
+            <MDBCardImage src={Image6} alt="Chore It Up" position="top" />
+            <MDBCardBody>
+              <MDBCardTitle>Chore It Up</MDBCardTitle>
+              <MDBCardText>
+                Chore It Up is a MERN stack application tailored for adults
+                where users can track the chores they are assigned to do around
+                the house. After a user signs up, one user can then create a
+                group and invite other users to that group. Once people join
+                they can assign each other chores that need to be done, update
+                the chore, or mark it as complete.
+              </MDBCardText>
+            </MDBCardBody>
+            <MDBBtn
+              onClick={() =>
+                openNewTab("https://floating-atoll-58183.herokuapp.com/")
               }
             >
               View App
